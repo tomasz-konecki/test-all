6fec930a052c2248f9f764509fc0a78470eb79c9
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-03-25 14:42:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends React.Component {
+	render() {
+		return (
+			<code>
+				<h1>React App</h1>
+			</code>
+		);
+	}
+}
+
+export default App;
