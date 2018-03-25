9ee7e7896d63e20c8b92bfd4dc23695a17a89a30
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-03-25 15:03:42 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
-import store from "./store";
-import { albumReleased } from "./actions/albumsActions";
+import _ from "lodash";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			album: "",
-			done: false
+			array: [
+				{ firstName: "Neil", middleName: "Francis", surname: "Tennant" },
+				{ firstName: "Chris", middleName: "Sean", surname: "Lowe" }
+			]
 		};
 	}
-
-	handleChange = (e) => {
-		this.setState({
-			album: e.target.value
-		});
-	};
-
-	onSubmit = () => {
-		this.setState({
-			done: true
-		});
-		store.dispatch(albumReleased(this.state.album));
-	};
-
-	handleKeyUp = (e) => {
-		if (e.keyCode === 13) this.onSubmit();
-	};
-
-	getAlbums = () => {
-		return store.getState().albums.map((album, index) => <li key="index">{album}</li>);
-	};
-
 	render() {
+		console.log(_.map(this.state.array, "surname"));
 		return (
 			<div>
-				<h2>PSB ALBUMS</h2>
-				<ul>{this.getAlbums()}</ul>
-				<hr />
-				<code>Enter album name:</code>
-				<input type="text" onChange={this.handleChange} onKeyUp={this.handleKeyUp} />
-				<button onClick={this.onSubmit}>Submit</button>
-				<hr />
-				{this.state.done === true && (
-					<code>
-						You entered:<span>
-							<b>{this.state.album}</b>
-						</span>
-					</code>
-				)}
+				<code>
+					<h2>Loadash</h2>
+				</code>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import store from "./store";
-import { albumReleased } from "./actions/albumsActions";
-
-store.dispatch(albumReleased("Please"));
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
